bc0097112de07523987d020fee424ce8597ae49a
MSW-yar <apnashery96@yahoo.com>
2019-03-04 17:57:14 +0500

    firstjs kaam

---
+++ 4-03-19/index.js
@@ -0,0 +1,73 @@
+// var a = "Hello"; //variable assigning
+// console.log(a); //printing actually
+// let b = 13;
+// console.log(b);
+//
+// let c = "2";
+// let d = "2";
+//
+// console.log(c + d); //typecatsting availabe and minus mein it gives answer and string converted into int if convertable
+// console.log(c - d);
+//
+// {
+//   let a = 2;
+//   console.log(a); // {local banata hai}
+// }
+// const dado = [1, 1, 3, 4, 5, 6];
+// dado[3] = 99;
+// console.log(dado);
+//
+// function baby(a, b) {
+//   // 1st tareeqa of func named definition
+//   return a + b;
+// }
+//
+// let x = baby(3, 8);
+// console.log(x);
+//
+// let y = function(a, b) {
+//   // 2nd tareeqa of func named expression
+//   console.log(a, b);
+// };
+//
+// y(4, 8);
+//
+// let n = () => console.log("baby"); // 3rd tareeqa of func named lambda
+// n();
+//   //4th is constructon()
+// (function(a, b) {
+//     console.log(a + b);
+//   }
+// )(7, 8); // instantlu evoked baby
+//
+// ((m,n)=> console.log(m+n))(9,0);
+//
+//
+//
+// ((k,l) => console.log(k+l))([1,2,3,4,5],[6,3,2,1,4])
+//
+//
+// let i = 0;
+// while (i < 10) {
+//   i += 1;
+//   console.log(i);
+// }
+// i = 0;
+// while (i < 100) {
+//   i += 1;
+//   console.log(i);
+// }
+//
+// for (let i = 01; i < 10; i += 0.5) {
+//   console.log(i);
+// }
+//
+// let l = [1, 2, 3, 4, 5];
+// for (let i in l) {
+//   console.log(l[i]);
+// }
+let i = 0
+do {
+  console.log(3 + 3);
+  i+=1
+} while (i<10)
